2c0fad054a1d1ef2568ce39c8150efb1508fba79
Facundo Campi <facucampi@gmail.com>
2018-10-14 01:20:36 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+
+app.set('view engine', 'hbs');
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+
+})
+
+hbs.registerHelper('screamIt', (txt) => {
+    return txt.toUpperCase();
+})
+
+app.use( (req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method}, ${req.url} -`
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (e) => {
+        if (e){
+            console.log(e)
+        }
+    })
+    next();
+});
+
+// app.use( (req, res, next)=> {
+//     res.render('maintenance.hbs')
+
+// }) 
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', (req, res) => {
+
+    res.render('home.hbs', {
+
+        pageTitle : 'Home Page',
+        welcomeText : 'Welcome to the Homepage'
+    });
+
+});
+
+app.get('/about', (req, res) => {
+
+    res.render('about.hbs', {
+
+        pageTitle : 'About Page'
+    })
+
+});
+
+app.get('/bad', (req, res) => {
+
+    res.send({
+        code : 'ERROR CATASTROFICO'
+    });
+
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
+
